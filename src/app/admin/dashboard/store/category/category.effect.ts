d0f5cbfcc38187e1378b1f2c620929cfcf42d04c
ngocphu97 <ngocphu2320@gmail.com>
2019-11-19 12:01:47 +0700

    Refactor code

---
+++ src/app/admin/dashboard/store/category/category.effect.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+
+import { Actions, ofType, createEffect } from '@ngrx/effects';
+import { of } from 'rxjs';
+import {  catchError, exhaustMap, map } from 'rxjs/operators';
+
+import * as fromActions from './category.actions'
+import { DashboardService } from '../../services/dashboard.service';
+
+// import { MatSnackBar } from '@angular/material';
+
+@Injectable()
+export class CategoriesEffect {
+
+  getCategoriesList$ = createEffect(() => this.actions$.pipe(
+    ofType(fromActions.getCategoriesList),
+    exhaustMap(() => {
+      return this.service.getBookCategories().pipe(
+        map((res: any) => {
+          return fromActions.getCategoriesListSuccess({ categoriesList: res })
+        }),
+        catchError(error => of(fromActions.getCategoriesListFail({ error: error })))
+      );
+    })
+  ));
+
+  getCategoriesForAmount$ = createEffect(() => this.actions$.pipe(
+    ofType(fromActions.getCategoryForAmount),
+    exhaustMap(() => {
+      return this.service.getCategoryForAmount().pipe(
+        map((res: any) => {
+          console.log(res);
+          return fromActions.getCategoryForAmountSuccess({ categoryForAmountList: res })
+        }),
+        catchError(error => of(fromActions.getCategoriesListFail({ error: error })))
+      );
+    })
+  ));
+
+  constructor(
+    private actions$: Actions,
+    private service: DashboardService,
+    // private snackBar: MatSnackBar
+  ) { }
+
+  // private openSnackBar(message: string, action: string): void {
+  //   this.snackBar.open(message, action, {
+  //     duration: 2000,
+  //   });
+  // }
+
+}
---
+++ src/app/admin/dashboard/store/category/category.selector.ts
@@ -0,0 +1,38 @@
+import { createSelector } from '@ngrx/store';
+
+import * as fromReducer from './category.reducer';
+import { selectAdminState, AdminState } from '../../../reducers';
+
+export const selectCategoriesState = createSelector(
+  selectAdminState,
+  (state: AdminState) => state.categories
+);
+
+export const selectCategoriesEntities = createSelector(
+  selectCategoriesState,
+  fromReducer.selectCatEntities
+);
+
+export const selectCategoriesList = createSelector(
+  selectCategoriesState,
+  fromReducer.selectAllCat
+);
+
+export const selectCategoriesForAmount = createSelector(
+  selectCategoriesState,
+  fromReducer.getSelectedCategoryForAmount
+);
+
+export const selectSelectedCategoriesId = createSelector(
+  selectCategoriesState,
+  fromReducer.getSelectedCategoriesId
+);
+
+export const selectCurrentCategories = createSelector(
+  selectCategoriesEntities,
+  selectSelectedCategoriesId,
+  (categoriesEntities, catId) => {
+    return categoriesEntities[catId]
+  }
+);
+
